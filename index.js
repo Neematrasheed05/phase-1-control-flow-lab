e154a3c5591ecd812bc49637690839d9336eacf2
Neematrashid <nymatrasheed05@gmail.com>
2023-09-19 06:18:34 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400) {
+    return 'This one is on me!'
+  } else if (x > 2000 && x < 2500) { 
+    return 'I will gladly take your thirty bucks.'
+  }
+  else
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  let Answer
+  city == 'NYC' ? Answer = 'Ok, sounds good.' : Answer = 'No go.';
+  return Answer;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+    break;
+    case 'not as generous':
+    return 'Thank you.'
+    break;
+    default:
+    return 'Bye.'
+    break;
+  }
 }
